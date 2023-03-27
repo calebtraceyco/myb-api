2199d53d5931d5ce55ce5e993cc7999546c0ed8f
Caleb Tracey <caleb.tracey@gmail.com>
2023-03-26 21:47:42 -0400

    FEAT-2 query parsing. handleFieldString working state

--- internal/dao/user/mapper.go
+++ internal/dao/user/mapper.go
@@ -2,9 +2,7 @@ package user
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/calebtraceyco/mind-your-business-api/external/models"
-	"github.com/calebtraceyco/mind-your-business-api/internal/dao/psql"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/exp/slices"
 	"reflect"
@@ -23,56 +21,81 @@ func (m Mapper) MapUserExec(user *models.User) (string, error) {
 	if reflect.ValueOf(user).IsNil() {
 		panic(MapUserExecError)
 	}
-	columnNames, columnValues := parseStructToSlices(user)
-	return fmt.Sprintf(psql.InsertExec, "users", columnNames, columnValues), nil
+	mapStruct := m.parseStructToSlices(user)
+	log.Infoln(mapStruct)
+	//
+	//for name, value := range mapStruct {
+	//	//TODO parse values and
+	//	//log.Infoln("field name: %v\n value: %v", name, value)
+	//}
+	return "", nil
+	//return fmt.Sprintf(psql.InsertExec, "users", columnNames, columnValues), nil
 }
 
-func parseStructToSlices(obj any) (string, string) {
+type MapFields map[string]any
+
+type MapStruct struct {
+	Type   reflect.Type
+	Value  reflect.Value
+	Obj    any
+	Fields MapFields
+}
+
+func (m Mapper) parseStructToSlices(obj any) MapFields {
 	var fieldNames, values []string
 
-	fieldMap := make(map[string]any)
+	mapStruct := &MapStruct{
+		Type:   reflect.TypeOf(obj),
+		Value:  reflect.ValueOf(obj),
+		Obj:    m.dereferencePointer(obj),
+		Fields: make(map[string]any),
+	}
+	//obj = m.dereferencePointer(obj)
+	//t := reflect.TypeOf(obj)
+	//v := reflect.ValueOf(obj)
 
-	obj = dereferencePointer(obj)
-	t := reflect.TypeOf(obj)
-	v := reflect.ValueOf(obj)
-
-	for i := 0; i < t.NumField(); i++ {
-		field := v.Field(i)
+	for i := 0; i < mapStruct.Type.NumField(); i++ {
+		field := mapStruct.Value.Field(i)
 		// get the field name for struct value with 'db' tag
-		fieldName := t.Field(i).Tag.Get(DatabaseStructTag)
+		fieldName := mapStruct.Type.Field(i).Tag.Get(DatabaseStructTag)
 
 		if field.IsValid() && fieldName != "" && !slices.Contains(excludedTags, fieldName) {
 			switch field.Kind() {
 			case reflect.String:
-				fieldMap = handleFieldString(field, fieldName, fieldMap)
+				m.handleFieldString(fieldName, field, mapStruct)
 			case reflect.Struct:
-				fieldNames, values = handleFieldStruct(field, fieldName, fieldNames, values)
+				fieldNames, values = m.handleFieldStruct(field, fieldName, fieldNames, values)
 			case reflect.Slice:
-				fieldNames, values = handleFieldSlice(field, fieldName, fieldNames, values)
+				fieldNames, values = m.handleFieldSlice(field, fieldName, fieldNames, values)
 			default:
 				continue
 			}
 		}
 	}
 
-	return strings.Join(fieldNames, ", "), strings.Join(values, ", ")
+	return mapStruct.Fields
 }
 
-func handleFieldString(field reflect.Value, fieldName string, fieldMap map[string]any) map[string]any {
+func parseField() {
+
+}
+
+func (m Mapper) handleFieldString(fieldName string, field reflect.Value, mapStruct *MapStruct) {
 	//fieldNames = append(fieldNames, fieldName)
-	if value, found := fieldMap[fieldName]; found {
-		value = strings.Join([]string{value.(string), wrapInSingleQuotes(field.String())}, ", ")
+	if value, found := mapStruct.Fields[fieldName]; found {
+		value = strings.Join([]string{value.(string), m.wrapInSingleQuotes(field.String())}, ", ")
+	} else {
+		log.Errorf("handleFieldString: field not found: %s", fieldName)
 	}
 	//values = append(values, wrapInSingleQuotes(field.String()))
-	return fieldMap
 }
 
 // handleFieldStruct turns a nested struct into a string to be stored as jsonb in postgres
 // TODO this seems bad since theres no validation for the json value being stored
-func handleFieldStruct(field reflect.Value, tag string, tags, values []string) ([]string, []string) {
+func (m Mapper) handleFieldStruct(field reflect.Value, tag string, tags, values []string) ([]string, []string) {
 	if data, err := json.Marshal(field.Interface()); err == nil {
 		tags = append(tags, tag)
-		values = append(values, wrapInSingleQuotes(string(data)))
+		values = append(values, m.wrapInSingleQuotes(string(data)))
 	} else if err != nil {
 		log.Errorf("handleFieldStruct: error: %v", err)
 	}
@@ -80,25 +103,25 @@ func handleFieldStruct(field reflect.Value, tag string, tags, values []string) (
 }
 
 // handleFieldSlice is a mess
-func handleFieldSlice(field reflect.Value, tag string, tags, values []string) ([]string, []string) {
+func (m Mapper) handleFieldSlice(field reflect.Value, tag string, tags, values []string) ([]string, []string) {
 	for i := 0; i < field.Len(); i++ {
 		subValue := field.Index(i)
 		if subValue.IsValid() && subValue.Kind() == reflect.String {
 			tags = append(tags, tag)
-			values = append(values, wrapInSingleQuotes(subValue.String()))
+			values = append(values, m.wrapInSingleQuotes(subValue.String()))
 		}
 	}
 	return []string{tag}, values
 }
 
-func dereferencePointer(obj any) any {
+func (m Mapper) dereferencePointer(obj any) any {
 	if reflect.ValueOf(obj).Kind() == reflect.Pointer {
 		obj = reflect.ValueOf(obj).Elem().Interface()
 	}
 	return obj
 }
 
-func wrapInSingleQuotes(s string) string {
+func (m Mapper) wrapInSingleQuotes(s string) string {
 	return "'" + strings.ReplaceAll(s, "'", "\\'") + "'"
 }
 
